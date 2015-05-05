4ebafe1f8b7f118f639d002c904460eab5680688
Jacky Boen <jacky.boen@bartleboglehegarty.com>
2015-05-05 19:23:23 +0800

    Don't open connection if the file is already downloaded
    Signed-off-by: Jacky Boen <jacky.boen@bartleboglehegarty.com>

--- net/net.go
+++ net/net.go
@@ -15,14 +15,9 @@ func Download(destDir, src, name string) error {
 	var err error
 	var n int64
 
-	// setup a connection to the file
-	if resp, err = http.Get(src); err != nil {
-		return fmt.Errorf("DownloadAs: %v", err)
-	}
-
 	// prepare directory
 	if err = os.MkdirAll(destDir, os.ModeDir | 0700); err != nil {
-		return fmt.Errorf("DownloadAs: %v", err)
+		return fmt.Errorf("Download: %v", err)
 	}
 
         // use different name if available
@@ -36,15 +31,20 @@ func Download(destDir, src, name string) error {
 		return nil
 	}
 
+	// setup a connection to the file
+	if resp, err = http.Get(src); err != nil {
+		return fmt.Errorf("Download: %v", err)
+	}
+
 	// create destination file to download to
 	if file, err = os.Create(destDir + "/" + name); err != nil {
-		return fmt.Errorf("DownloadAs: %v", err)
+		return fmt.Errorf("Download: %v", err)
 	}
 	defer file.Close()
 
 	// start download
 	if n, err = io.Copy(file, resp.Body); err != nil {
-                return fmt.Errorf("DownloadAs: %v", err)
+                return fmt.Errorf("Download: %v", err)
 	}
 
 	log.Printf("downloaded %s (%dKB)\n", name, n / 1024)
